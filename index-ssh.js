4e0f9b24041f64c3834373978a552a6bff961784
kalvish <kalanvishwanath@gmail.com>
2016-07-10 15:29:17 +0530

    added one more client

--- index-ssh.js
+++ index-ssh.js
@@ -97,6 +97,12 @@ io.sockets.on('connection', function (socket) {
           socket.join(room);
           socket.emit('joined', room, socket.id);
           io.sockets.in(room).emit('ready');
+        } else if (numClients === 3) {
+          console.log('Client ID ' + socket.id + ' joined room ' + room);
+          io.sockets.in(room).emit('join', room);
+          socket.join(room);
+          socket.emit('joined', room, socket.id);
+          io.sockets.in(room).emit('ready');
         } else { // max two clients
           socket.emit('full', room);
         }
