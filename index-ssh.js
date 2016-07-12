0ea35f5c25e8e41a49fc2bf18809dfcac062e2dc
kalvish <kalanvishwanath@gmail.com>
2016-07-13 01:05:42 +0530

    gitignore package change

--- index-ssh.js
+++ index-ssh.js
@@ -256,11 +256,11 @@ function sendMessage(message) {
 }
 
   
-//   socket.on('message-peer', function(message) {
-//   //console.log('Client received message ', message);
-//   console.log('Client-peer received message ', message);
-//   socket.emit('create or join', "temproom");
-// });
+  socket.on('message-peer', function(message) {
+  //console.log('Client received message ', message);
+  console.log('Client-peer received message ', message);
+  socket.emit('create or join', "temproom");
+});
 
   function createPeerConnection(isInitiator, config) {
     console.log('Creating Peer connection as initiator?', isInitiator, 'config:',
