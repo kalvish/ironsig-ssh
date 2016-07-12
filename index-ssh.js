3471ab72c2ada20a413522cef8f024513b27f1b1
kalvish <kalanvishwanath@gmail.com>
2016-07-12 23:54:50 +0530

    http edits

--- index-ssh.js
+++ index-ssh.js
@@ -194,11 +194,165 @@ app.listen(8080);
 
 var socket = require('socket.io-client')('http://localhost:8080');
 //var socket = require('socket.io-client')('https://localhost',{secure: true, port:8080});
-  socket.on('connect', function(){});
-  socket.on('event', function(data){});
-  socket.on('disconnect', function(){});
+  //socket.on('connect', function(){});
+  //socket.on('event', function(data){});
+  //socket.on('disconnect', function(){});
+
+  socket.on('created', function(room, clientId) {
+  isInitiator = true;
+  console.log('Created room', room, '- my client ID is', clientId);
+  //grabWebCamVideo();
+  createPeerConnection(isInitiator, configuration);
+  //createSimplePeer(isInitiator, configuration);
+});
+
+socket.on('full', function(room) {
+  console.log('Message from client: Room ' + room + ' is full :^(');
+});
+
+socket.on('ipaddr', function(ipaddr) {
+  console.log('Message from client: Server IP address is ' + ipaddr);
+});
+
+socket.on('joined', function(room, clientId) {
+  isInitiator = false;
+  console.log('This peer has joined room', room, 'with client ID', clientId);
+  //grabWebCamVideo();
+  createPeerConnection(isInitiator, configuration);
+  //createSimplePeer(isInitiator, configuration);
+});
+
+socket.on('ready', function() {
+  console.log('Socket is ready');
+  createPeerConnection(isInitiator, configuration);
+  //createSimplePeer(isInitiator, configuration);
+});
+
+socket.on('message', function(message) {
+  //console.log('Client received message ', message);
+  console.log('Client received message ', message);
+  
+  
+  //signalingMessageCallback(message);
+  if(message.candidate){
+
+    var candTemp = { candidate : {
+      candidate: message.candidate
+    }};
+    p.signal(candTemp);
+  }else{
+    p.signal(message);
+  }
+  
+});
+
+/**
+* Send message to signaling server
+*/
+function sendMessage(message) {
+  console.log('Client sending message with type ', message.type);
+  socket.emit('message', message);
+}
+
   socket.emit('create or join', "temproom");
   socket.on('message-peer', function(message) {
   //console.log('Client received message ', message);
   console.log('Client-peer received message ', message);
 });
+
+  function createPeerConnection(isInitiator, config) {
+    console.log('Creating Peer connection as initiator?', isInitiator, 'config:',
+              config);
+    //peerConn = new RTCPeerConnection(config);
+    if(isInitiator){
+
+
+    p = new Peer({ initiator: true, trickle: false });
+  }else{
+    p = new Peer({ initiator: false, trickle: false });
+  }
+  // send any ice candidates to the other peer
+  // peerConn.onicecandidate = function(event) {
+  //   console.log('icecandidate event:', event);
+  //   if (event.candidate) {
+  //     sendMessage({
+  //       type: 'candidate',
+  //       label: event.candidate.sdpMLineIndex,
+  //       id: event.candidate.sdpMid,
+  //       candidate: event.candidate.candidate
+  //     });
+  //   } else {
+  //     console.log('End of candidates.');
+  //   }
+  // };
+  // p.onicecandidate = function(event) {
+  //   console.log('icecandidate event:', event);
+  //   if (event.candidate) {
+  //     sendMessage({
+  //       type: 'candidate',
+  //       label: event.candidate.sdpMLineIndex,
+  //       id: event.candidate.sdpMid,
+  //       candidate: event.candidate.candidate
+  //     });
+  //   } else {
+  //     console.log('End of candidates.');
+  //   }
+  // };
+
+  p.on('signal', function (data) {
+    sendMessage(data);
+    console.log('SIGNAL', data)
+  })
+
+  if (isInitiator) {
+
+    console.log('Creating Data Channel');
+    // dataChannel = peerConn.createDataChannel('photos');
+    // onDataChannelCreated(dataChannel);
+
+    // console.log('Creating an offer');
+    // peerConn.createOffer(onLocalSessionCreated, logError);
+
+    //dataChannel = p.createDataChannel('photos');
+
+    onDataChannelCreated(dataChannel);
+
+    console.log('Creating an offer');
+    //p.createOffer(onLocalSessionCreated, logError);
+  } else {
+    // peerConn.ondatachannel = function(event) {
+    //   console.log('ondatachannel:', event.channel);
+    //   dataChannel = event.channel;
+    //   onDataChannelCreated(dataChannel);
+    // };
+    //  p.ondatachannel = function(event) {
+    //   console.log('ondatachannel:', event.channel);
+    //   dataChannel = event.channel;
+    //   onDataChannelCreated(dataChannel);
+    // };
+
+    onDataChannelCreated(dataChannel);
+  }
+}
+
+function onDataChannelCreated(channel) {
+  console.log('onDataChannelCreated:', channel);
+
+  // channel.onopen = function() {
+  //   console.log('CHANNEL opened!!!');
+  // };
+  p.on('connect', function () {
+    var toSend = Math.random();
+    console.log('CONNECT and Send' + toSend);
+    p.send('whatever' + toSend);
+  })
+
+  //channel.onmessage = (adapter.browserDetails.browser === 'firefox') ?
+  //receiveDataFirefoxFactory() : receiveDataChromeFactory();
+
+  p.on('data', function (data) {
+    //(adapter.browserDetails.browser === 'firefox') ?
+  //receiveDataFirefoxFactory() : receiveDataChromeFactory(data);
+    console.log('data: ' + data)
+  })
+}
