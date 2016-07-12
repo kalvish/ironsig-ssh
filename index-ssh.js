94104742b126fd3a7084f6a4c22d887a1946b855
kalvish <kalanvishwanath@gmail.com>
2016-07-12 23:49:24 +0530

    http edits

--- index-ssh.js
+++ index-ssh.js
@@ -1,9 +1,9 @@
-// var isInitiator = false;
+var isInitiator = false;
 
-// var configuration = null;
+var configuration = null;
 
-// var Peer = require('simple-peer')
-// var p;
+var Peer = require('simple-peer')
+var p;
 
 
 var fs = require('fs');
@@ -18,8 +18,8 @@ var options = {
     cert: fs.readFileSync('fake-keys/certificate.pem')
 };
 
-//var app = require('http').createServer(serverCallback);
-var app = require('https').createServer(options, serverCallback);
+var app = require('http').createServer(serverCallback);
+//var app = require('https').createServer(options, serverCallback);
 
 function serverCallback(request, response) {
     request.addListener('end', function () {
@@ -192,7 +192,8 @@ app.listen(8080);
 //   console.log('Client-peer received message ', message);
 // });
 
-var socket = require('socket.io-client')('https://localhost',{secure: true, port:8080});
+var socket = require('socket.io-client')('http://localhost:8080');
+//var socket = require('socket.io-client')('https://localhost',{secure: true, port:8080});
   socket.on('connect', function(){});
   socket.on('event', function(data){});
   socket.on('disconnect', function(){});
