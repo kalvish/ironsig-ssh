20852adc39cd4a97ac2c00eb57f0b204afa415f1
ganuka <ganuke@Krishans-Mac-mini.local>
2016-07-16 11:36:42 +0530

    emit create conn

--- index-ssh.js
+++ index-ssh.js
@@ -231,6 +231,8 @@ var socket = require('socket.io-client')('http://localhost:8080');
   //socket.on('event', function(data){});
   //socket.on('disconnect', function(){});
 
+socket.emit('create or join', 'testroom1');
+
   socket.on('created', function(room, clientId) {
   isInitiator = true;
   console.log('Created room', room, '- my client ID is', clientId);
