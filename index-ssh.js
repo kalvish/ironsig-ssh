7dcbacc005462faefb74c3cf18cc15f790a12cb8
ganuka <ganuke@Krishans-Mac-mini.local>
2016-07-16 12:31:46 +0530

    http server forwading to https

--- index-ssh.js
+++ index-ssh.js
@@ -19,8 +19,8 @@ var options = {
     cert: fs.readFileSync('fake-keys/certificate.pem')
 };
 
-//var app = require('http').createServer(serverCallback);
-var app = require('https').createServer(options, serverCallback);
+var app = require('http').createServer(serverCallback);
+//var app = require('https').createServer(options, serverCallback);
 
 function serverCallback(request, response) {
     request.addListener('end', function () {
@@ -195,8 +195,10 @@ function onNewNamespace(channel, sender) {
     });
 }
 
-app.listen(8080);
+app.listen(3001);
 
+var server = https.createServer(options, app);
+server.listen(8080);
 // var io = require('socket.io');
 // var socket = io.connect('https://localhost:8080');
 // socket.on('news', function (data) {
@@ -225,9 +227,9 @@ app.listen(8080);
 //   console.log('Client-peer received message ', message);
 // });
 
-//var socket = require('socket.io-client')('http://localhost:8080');
+var socket = require('socket.io-client')('http://localhost:8080');
 //var socket = require('socket.io-client')('https://localhost',{secure: true, port:8080});
-var socket = require('socket.io-client')('https://localhost:8080');
+//var socket = require('socket.io-client')('https://localhost:8080');
   //socket.on('connect', function(){});
   //socket.on('event', function(data){});
   //socket.on('disconnect', function(){});
