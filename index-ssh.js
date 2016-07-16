baa686629516df96544b45a8a6ebbbb40da89ca3
ganuka <ganuke@Krishans-Mac-mini.local>
2016-07-16 12:09:19 +0530

    socket io to https

--- index-ssh.js
+++ index-ssh.js
@@ -225,8 +225,8 @@ app.listen(8080);
 //   console.log('Client-peer received message ', message);
 // });
 
-var socket = require('socket.io-client')('http://localhost:8080');
-//var socket = require('socket.io-client')('https://localhost',{secure: true, port:8080});
+//var socket = require('socket.io-client')('http://localhost:8080');
+var socket = require('socket.io-client')('https://localhost',{secure: true, port:8080});
   //socket.on('connect', function(){});
   //socket.on('event', function(data){});
   //socket.on('disconnect', function(){});
