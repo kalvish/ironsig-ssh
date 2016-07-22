69788c15b7aef04e78ee861eec6f6d644ec03028
ganuka <ganuke@Krishans-Mac-mini.local>
2016-07-22 15:01:22 +0530

    clients increased just to test

--- index-ssh.js
+++ index-ssh.js
@@ -105,18 +105,18 @@ io.sockets.on('connection', function (socket) {
         var numClients = io.sockets.sockets.length;
         console.log('Room ' + room + ' now has ' + numClients + ' client(s)');
 
-        if (numClients === 1) {
+        if (numClients === 2) {
           socket.join(room);
           console.log('Client ID ' + socket.id + ' created room ' + room);
           socket.emit('created', room, socket.id);
 
-        } else if (numClients === 2) {
+        } else if (numClients === 3) {
           console.log('Client ID ' + socket.id + ' joined room ' + room);
           io.sockets.in(room).emit('join', room);
           socket.join(room);
           socket.emit('joined', room, socket.id);
           io.sockets.in(room).emit('ready');
-        } else if (numClients === 3) {
+        } else if (numClients === 4) {
           console.log('Client ID ' + socket.id + ' joined room ' + room);
           io.sockets.in(room).emit('join', room);
           socket.join(room);
